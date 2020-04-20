2b846c5b95b74ff88a421812bfa631669d137377
施泽西 <shizexi@coding.com>
2020-04-20 17:12:45 +0800

    refactor(tencentcloud): refactor tencentcloud rollback cluster stage angular component to react component

---
+++ app/scripts/modules/tencentcloud/src/pipeline/stages/rollbackCluster/RollbackClusterStage.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import { AccountService, IStageConfigProps, NgReact } from '@spinnaker/core';
+import { get } from 'lodash';
+const { AccountRegionClusterSelector } = NgReact;
+
+interface IProps extends IStageConfigProps {}
+interface IState {
+  accounts: any[];
+  regionsLoaded: boolean;
+}
+
+export class RollbackClusterStage extends React.Component<IProps, IState> {
+  public state: IState = {
+    accounts: [],
+    regionsLoaded: false,
+  };
+
+  public componentDidMount() {
+    const { application, stage, updateStageField } = this.props;
+    AccountService.listAccounts('tencentcloud').then(accounts => {
+      this.setState({
+        accounts,
+      });
+    });
+    updateStageField({
+      cloudProvider: 'tencentcloud',
+      targetHealthyRollbackPercentage: stage.targetHealthyRollbackPercentage || 100,
+      regions: stage.regions || [],
+    });
+
+    if (
+      stage.isNew &&
+      application.attributes.platformHealthOnlyShowOverride &&
+      application.attributes.platformHealthOnly
+    ) {
+      updateStageField({
+        interestingHealthProviderNames: ['Tencent'],
+      });
+    }
+
+    if (!stage.credentials && application.defaultCredentials.tencentcloud) {
+      stage.credentials = application.defaultCredentials.tencentcloud;
+      updateStageField({
+        credentials: application.defaultCredentials.tencentcloud,
+      });
+    }
+    if (!get(stage, 'regions', []).length && application.defaultRegions.tencentcloud) {
+      updateStageField({
+        regions: [...stage.regions, application.defaultCredentials.tencentcloud],
+      });
+    }
+  }
+
+  public render() {
+    const { application, pipeline, stage, updateStageField } = this.props;
+    const { accounts } = this.state;
+    return (
+      <div className="form-horizontal">
+        {!pipeline.strategy && (
+          <AccountRegionClusterSelector
+            application={application}
+            clusterField={'cluster'}
+            component={stage}
+            accounts={accounts}
+          />
+        )}
+        <div className="row">
+          <div className="col-sm-10 col-sm-offset-2">
+            Wait
+            <input
+              type="number"
+              min="0"
+              value={stage.waitTimeBetweenRegions}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                updateStageField({
+                  waitTimeBetweenRegions: Number(e.target.value),
+                })
+              }
+              className="form-control input-sm inline-number"
+              style={{ margin: '0 5px' }}
+            />
+            seconds between regional rollbacks.
+          </div>
+          <div className="col-sm-10 col-sm-offset-2">
+            Consider rollback successful when
+            <input
+              type="number"
+              min="0"
+              max="100"
+              value={stage.targetHealthyRollbackPercentage}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                updateStageField({
+                  targetHealthyRollbackPercentage: Number(e.target.value),
+                })
+              }
+              className="form-control input-sm inline-number"
+              style={{ margin: '0 5px' }}
+            />
+            percent of instances are healthy.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- app/scripts/modules/tencentcloud/src/pipeline/stages/rollbackCluster/tencentCloudRollbackClusterStage.js
+++ app/scripts/modules/tencentcloud/src/pipeline/stages/rollbackCluster/tencentCloudRollbackClusterStage.js
@@ -1,74 +1,22 @@
 'use strict';
 
 const angular = require('angular');
+import { RollbackClusterStage } from './RollbackClusterStage';
 
-import { AccountService, Registry } from '@spinnaker/core';
+import { Registry } from '@spinnaker/core';
 
 export const TENCENT_PIPELINE_STAGES_ROLLBACKCLUSTER_TENCENTCLOUDROLLBACKCLUSTERSTAGE =
-  'spinnaker.tencentcloud.pipeline.stage.rollbackClusterStage';
-angular
-  .module(TENCENT_PIPELINE_STAGES_ROLLBACKCLUSTER_TENCENTCLOUDROLLBACKCLUSTERSTAGE, [])
-  .config(function() {
-    Registry.pipeline.registerStage({
-      provides: 'rollbackCluster',
-      cloudProvider: 'tencentcloud',
-      templateUrl: require('./rollbackClusterStage.html'),
-      validators: [
-        {
-          type: 'requiredField',
-          fieldName: 'cluster',
-        },
-        {
-          type: 'requiredField',
-          fieldName: 'regions',
-        },
-        {
-          type: 'requiredField',
-          fieldName: 'credentials',
-          fieldLabel: 'account',
-        },
-      ],
-    });
-  })
-  .controller('tencentRollbackClusterStageCtrl', [
-    '$scope',
-    function($scope) {
-      const ctrl = this;
-
-      const stage = $scope.stage;
-
-      $scope.state = {
-        accounts: false,
-        regionsLoaded: false,
-      };
-
-      AccountService.listAccounts('tencentcloud').then(function(accounts) {
-        $scope.accounts = accounts;
-        $scope.state.accounts = true;
-      });
-
-      ctrl.reset = () => {
-        ctrl.accountUpdated();
-        ctrl.resetSelectedCluster();
-      };
-
-      stage.regions = stage.regions || [];
-      stage.cloudProvider = 'tencentcloud';
-      stage.targetHealthyRollbackPercentage = stage.targetHealthyRollbackPercentage || 100;
-
-      if (
-        stage.isNew &&
-        $scope.application.attributes.platformHealthOnlyShowOverride &&
-        $scope.application.attributes.platformHealthOnly
-      ) {
-        stage.interestingHealthProviderNames = ['Tencentcloud'];
-      }
-
-      if (!stage.credentials && $scope.application.defaultCredentials.tencentcloud) {
-        stage.credentials = $scope.application.defaultCredentials.tencentcloud;
-      }
-      if (!stage.regions.length && $scope.application.defaultRegions.tencentcloud) {
-        stage.regions.push($scope.application.defaultRegions.tencentcloud);
-      }
-    },
-  ]);
+  'spinnaker.tencent.pipeline.stage.rollbackClusterStage';
+export const name = TENCENT_PIPELINE_STAGES_ROLLBACKCLUSTER_TENCENTCLOUDROLLBACKCLUSTERSTAGE; // for backwards compatibility
+angular.module(TENCENT_PIPELINE_STAGES_ROLLBACKCLUSTER_TENCENTCLOUDROLLBACKCLUSTERSTAGE, []).config(function() {
+  Registry.pipeline.registerStage({
+    provides: 'rollbackCluster',
+    cloudProvider: 'tencentcloud',
+    component: RollbackClusterStage,
+    validators: [
+      { type: 'requiredField', fieldName: 'cluster' },
+      { type: 'requiredField', fieldName: 'regions' },
+      { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account' },
+    ],
+  });
+});
